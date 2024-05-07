9a2ceed04da28aa00b906008b9f6ddff274fe29c
jhonattan2121 <jhoontutors@gmail.com>
2024-05-07 13:18:25 -0300

    correcao da base URL temporaria

--- src/http-common.ts
+++ src/http-common.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
-  baseURL: "http://192.168.42.25:2653",
+  baseURL: " https://zecfaucet.com:2653",
   headers: {
     "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
   }
